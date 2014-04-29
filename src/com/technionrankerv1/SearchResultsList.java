47365dbb7dcdf5632abe0cf6c8399dbc5cd3952d
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-04-29 18:37:04 +0300

    Cleaning up SearchResults code

--- src/com/technionrankerv1/SearchResultsList.java
+++ src/com/technionrankerv1/SearchResultsList.java
@@ -1,13 +1,9 @@
 package com.technionrankerv1;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
-
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -65,62 +61,41 @@ public class SearchResultsList extends ListActivity {
 		}
 	
 	public String[] parse() throws Exception { 
-		
-		HashMap<String,String> map = new HashMap<String,String>(); //create Hashmap, where the numbers are the keys and the Titles are the values
-		
+		 //create Hashmap, where the numbers are the keys and the Titles are the values
+		HashMap<String,String> map = new HashMap<String,String>();
 		String inputLine = "";
 		String[] temp;
-		HashSet toReturn = new HashSet<String>();
 		String[] allCourses = new String[0];
 		String[] courseFiles = getAssets().list("CourseListings");
 		for (int i = 0; i < courseFiles.length; i++) {
 			int lineNumber = 0;
 		    BufferedReader infile = new BufferedReader(new InputStreamReader(getAssets().open("CourseListings/" + courseFiles[i])));
-			//InputStream is = getResources().openRawResource(R.raw.aerospace_engineering);
-			//BufferedReader infile = new BufferedReader(new InputStreamReader(is)); // read the text file
 			while (infile.ready()) {// while more info exists
 				if(lineNumber>=13 && lineNumber%3==1){ // only take numbers 13 and up for every 3
 					temp = inputLine.split(" - ");
 					for (int t = 0; t<temp.length; t++){
-						//Log.d(getLocalClassName(), temp[0]);
 						String number = temp[0].trim();//number;
 						String name = temp[1].replaceAll("</A>","").trim();//name
 						map.put(number,name); // trim and place only the number and name in
-					}
-				}
+					} //for temp
+				} //if
 				inputLine = infile.readLine(); // read the next line of the text
 				lineNumber++;
-			}
-			
+			} //while infile
 			infile.close();	
-			
 			Object[] temp1 = map.values().toArray();
-			//Collection<String> temp1 = map.values();
 			String[] oneFacultyCourses = Arrays.copyOf(temp1, temp1.length, String[].class);
 			allCourses = concat(allCourses, oneFacultyCourses);
-		}
-		
+		} //for courseFiles
 		return allCourses;
-		/*
-		String[] toReturn = new String[5];
-		for (Object key : temp1) { // iterate through the map and print the keys and the values they map to
-		{	
-			toReturn[]
-			System.out.print(key+" ");
-			System.out.println(map.get(key));
-		}
-		*/
-	}
+	} //parse()
 	
 	String[] concat(String[] a, String[] b) {
-		   int aLen = a.length;
-		   int bLen = b.length;
-		   String[] c= new String[aLen+bLen];
-		   System.arraycopy(a, 0, c, 0, aLen);
-		   System.arraycopy(b, 0, c, aLen, bLen);
-		   return c;
-		}
-
-
-
+		int aLen = a.length;
+		int bLen = b.length;
+		String[] c= new String[aLen+bLen];
+		System.arraycopy(a, 0, c, 0, aLen);
+		System.arraycopy(b, 0, c, aLen, bLen);
+		return c;
+	}
 }
