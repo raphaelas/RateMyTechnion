d96e7104c1f6a2eb158143cd7fb53b522c9cef6f
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-05-07 23:15:00 +0300

    Autocomplete works, database doesn't

--- src/com/technionrankerv1/AutoCorrect.java
+++ src/com/technionrankerv1/AutoCorrect.java
@@ -1,11 +1,22 @@
 package com.technionrankerv1;
 
+import com.serverapi.TechnionRankerAPI;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class AutoCorrect extends Activity{
+public class AutoCorrect extends Activity implements OnClickListener {
+	Button bSend;
+	TextView tvStatus;
+	
 	protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.welcome_view);
@@ -15,10 +26,58 @@ public class AutoCorrect extends Activity{
         AutoCompleteTextView textView = (AutoCompleteTextView)
                 findViewById(R.id.autoCompleteView);
         textView.setAdapter(adapter);
+        
+	    tvStatus = (TextView) findViewById(R.id.tvStatus);
+	    bSend = (Button) findViewById(R.id.bSend);
+	    bSend.setOnClickListener(this);
     }
+	
+	public void showSearchResults(View view) {
+		Intent intent = new Intent(this, SearchResultsList.class);
+		startActivity(intent);
+		/*
+		 * From Android Developer's website: EditText editText = (EditText)
+		 * findViewById(R.id.edit_message); String message =
+		 * editText.getText().toString(); intent.putExtra(EXTRA_MESSAGE,
+		 * message);
+		 */
+	}
 
     private static final String[] ITEMS = new String[] {
         "Belgium", "France", "Italy", "Germany", "Spain"
     };
+    
+	@Override
+	public void onClick(View v) {
+		tvStatus.setText("wait..");
+		ClientAsync as = new ClientAsync();
+		as.execute("");
+	}
+	
+	private class ClientAsync extends AsyncTask<String, Void, String> {
 
+	    public ClientAsync() {
+	    }
+
+	    @Override
+	    protected void onPreExecute() {
+	      // TODO Auto-generated method stub
+	      super.onPreExecute();
+	      tvStatus.setText("wait..");
+	    }
+
+	    @Override
+	    protected String doInBackground(String... params) {
+	      Course c = new Course("01", "Geology", (long) 5, "Spring 2011", true);
+	      return new TechnionRankerAPI().insertCourse(c).toString();
+	    }
+
+	    @Override
+	    protected void onPostExecute(String res) {
+	      if (res == null)
+	        tvStatus.setText("null");
+	      else
+	        tvStatus.setText(res);
+	    }
+	  }
 }
--- src/com/technionrankerv1/MainActivity.java
+++ src/com/technionrankerv1/MainActivity.java
@@ -5,50 +5,16 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.serverapi.TechnionRankerAPI;
-
-public class MainActivity extends Activity implements OnItemClickListener, OnClickListener {
-	Button bSend;
-	TextView tvStatus;
-	
-	private class ClientAsync extends AsyncTask<String, Void, String> {
-
-	    public ClientAsync() {
-	    }
-
-	    @Override
-	    protected void onPreExecute() {
-	      // TODO Auto-generated method stub
-	      super.onPreExecute();
-	      tvStatus.setText("wait..");
-	    }
-
-	    @Override
-	    protected String doInBackground(String... params) {
-	      Course c = new Course("01", "Geology", (long) 5, "Spring 2011", true);
-	      return new TechnionRankerAPI().insertCourse(c).toString();
-	    }
-
-	    @Override
-	    protected void onPostExecute(String res) {
-	      if (res == null)
-	        tvStatus.setText("null");
-	      else
-	        tvStatus.setText(res);
-	    }
-	  }
+public class MainActivity extends Activity implements OnItemClickListener {
 
 	ListView listView;
 	ExpandableListAdapter listAdapter;
@@ -80,29 +46,12 @@ public class MainActivity extends Activity implements OnItemClickListener, OnCli
 		else if (viewToStartOn.equals("prof_view")){
 			setContentView(R.layout.prof_view);
 		}
-	    tvStatus = (TextView) findViewById(R.id.tvStatus);
-	    bSend = (Button) findViewById(R.id.bSend);
-	    bSend.setOnClickListener(this);
 	}
 
 	public void initUserCourseView(View view) {
 		Intent intent = new Intent(this, UserCourseViewMain.class);
 		startActivity(intent);
 	}
-
-	public void showSearchResults(View view) {
-		Intent intent = new Intent(this, SearchResultsList.class);
-		startActivity(intent);
-		/*
-		 * From Android Developer's website: EditText editText = (EditText)
-		 * findViewById(R.id.edit_message); String message =
-		 * editText.getText().toString(); intent.putExtra(EXTRA_MESSAGE,
-		 * message);
-		 */
-	}
-	
-
-	
 	
 	@Override
 	public void onItemClick(AdapterView<?> adapter, View view, int position,
@@ -110,13 +59,5 @@ public class MainActivity extends Activity implements OnItemClickListener, OnCli
 		Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
 				Toast.LENGTH_SHORT).show();
 	}
-
-	@Override
-	public void onClick(View v) {
-		tvStatus.setText("wait..");
-		ClientAsync as = new ClientAsync();
-		as.execute("");
-	}
-	
 	
 }
