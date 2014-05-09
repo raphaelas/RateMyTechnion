d678d55bb3660b7c83a4c7b5d7368b61aed00f70
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-05-10 01:12:59 +0300

    Added appcomcat

--- src/com/technionrankerv1/AutoCorrect.java
+++ src/com/technionrankerv1/AutoCorrect.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -106,7 +105,6 @@ public class AutoCorrect extends Activity implements OnClickListener {
 	public String[] parseCourses() throws Exception { 
 		 //create Hashmap, where the numbers are the keys and the Titles are the values
 		HashMap<String,String> map = new HashMap<String,String>();
-		HashSet<Course> set = new HashSet<Course>();
 		String inputLine = "";
 		String[] temp;
 		String[] courseFiles = getAssets().list("CourseListings");
@@ -120,7 +118,6 @@ public class AutoCorrect extends Activity implements OnClickListener {
 						String number = temp[0].trim();//number;
 						String name = temp[1].replaceAll("</A>","").trim();//name
 						map.put(number,name); // trim and place only the number and name in
-						set.add(new Course(name, number, null, null, true));
 					} //for temp
 				} //if
 				inputLine = infile.readLine(); // read the next line of the text
@@ -132,6 +129,7 @@ public class AutoCorrect extends Activity implements OnClickListener {
 		Object[] courseNameObjectArray = map.values().toArray();
 		String[] allCourses = Arrays.copyOf(courseNameObjectArray, courseNameObjectArray.length, String[].class);
 		String[] allNumbers = Arrays.copyOf(courseNumberObjectArray, courseNumberObjectArray.length, String[].class);
+		Log.d(getLocalClassName(), allNumbers.toString());
 		return allCourses;
 	} //parse()
 	
