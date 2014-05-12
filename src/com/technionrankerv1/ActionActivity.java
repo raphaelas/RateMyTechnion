69c7d2e206efe068a519289d5018ef342cd67152
Leonid Lantsman <leonidlan1992@gmail.com>
2014-05-12 15:38:44 +0300

    Figuring out appcomcat

--- src/com/technionrankerv1/ActionActivity.java
+++ src/com/technionrankerv1/ActionActivity.java
@@ -3,6 +3,7 @@ package com.technionrankerv1;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 
 public class ActionActivity extends ActionBarActivity {
@@ -12,5 +13,21 @@ public class ActionActivity extends ActionBarActivity {
 	    MenuInflater inflater = getMenuInflater();
 	    inflater.inflate(R.menu.main_activity_actions, menu);
 	    return super.onCreateOptionsMenu(menu);
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+		/*
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            openSearch();
+	            return true;
+	        case R.id.action_settings:
+	            openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }*/
+	    return true;	    
 	}
 }
