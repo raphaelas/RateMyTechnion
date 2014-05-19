288048b48cbf5741e97315d9b61c824dfe546920
Leonid Lantsman <leonidlan1992@gmail.com>
2014-05-19 04:47:48 +0300

    nothing is working

--- src/com/technionrankerv1/SearchableActivity.java
+++ src/com/technionrankerv1/SearchableActivity.java
@@ -5,6 +5,10 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 public class SearchableActivity extends ListActivity {
 
@@ -19,76 +23,76 @@ public class SearchableActivity extends ListActivity {
 		Intent intent = getIntent();
 		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
 			String query = intent.getStringExtra(SearchManager.QUERY);
-//			SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
-//	                MySuggestionProvider.AUTHORITY, MySuggestionProvider.MODE);
-//	        suggestions.saveRecentQuery(query, null);
-			Log.d("MyApp", "value:"+query);
+			// SearchRecentSuggestions suggestions = new
+			// SearchRecentSuggestions(this,
+			// MySuggestionProvider.AUTHORITY, MySuggestionProvider.MODE);
+			// suggestions.saveRecentQuery(query, null);
+			Log.d("MyApp", "value:" + query);
 			Bundle b = new Bundle();
 			b.putString("query", query);
-			Intent newIntent = new Intent(getApplicationContext(),SearchResults.class);
+			Intent newIntent = new Intent(getApplicationContext(),
+					SearchResults.class);
 			newIntent.putExtras(b);
 			startActivity(newIntent);
 		}
 
+		
+
 	}
 }
+
 /*
-	public class MyAdapter {
-		public MyAdapter(Context context, int layoutResourceId, String[] places) {
-			super(context, layoutResourceId, places);
-			this.context = context;
->>>>>>> origin/master
+ * public class MyAdapter { public MyAdapter(Context context, int
+ * layoutResourceId, String[] places) { super(context, layoutResourceId,
+ * places); this.context = context; >>>>>>> origin/master
+ * 
+ * // }
+ * 
+ * /* public void doMySearch(String query) { Context context=getBaseContext();
+ * SearchableAdapter sa=new SearchableAdapter(getBaseContext()); sa. }
+ */
+/*
+ * public class MyAdapter { public MyAdapter(Context context, int
+ * layoutResourceId, String[] places) { super(context, layoutResourceId,
+ * places); this.context = context;
+ * 
+ * this.data = Arrays.asList(places); this.origData = new
+ * ArrayList<String>(this.data);
+ * 
+ * } }
+ */
 
-		// }
-
-		/*
-		 * public void doMySearch(String query) { Context
-		 * context=getBaseContext(); SearchableAdapter sa=new
-		 * SearchableAdapter(getBaseContext()); sa. }
-		 */
-		/*
-		 * public class MyAdapter { public MyAdapter(Context context, int
-		 * layoutResourceId, String[] places) { super(context, layoutResourceId,
-		 * places); this.context = context;
-		 * 
-		 * this.data = Arrays.asList(places); this.origData = new
-		 * ArrayList<String>(this.data);
-		 * 
-		 * } }
-		 */
-
-	/*
+/*
+ * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the menu
+ * items for use in the action bar MenuInflater inflater = getMenuInflater();
+ * inflater.inflate(R.menu.main_activity_actions, menu); // Get the SearchView
+ * and set the searchable configuration MenuItem
+ * searchItem=menu.findItem(R.id.action_search); SearchView
+ * searchView=(SearchView)MenuItemCompat.getActionView(searchItem);
+ * SearchManager searchManager = (SearchManager)
+ * getSystemService(Context.SEARCH_SERVICE); //SearchView searchView =
+ * (SearchView) menu.findItem(R.id.action_search) // .getActionView(); //
+ * Assumes current activity is the searchable activity
+ * searchView.setSearchableInfo(searchManager
+ * .getSearchableInfo(getComponentName()));
+ * searchView.setIconifiedByDefault(false); // Do not iconify the widget; //
+ * expand it by default // searchView.setSubmitButtonEnabled(true); return true;
+ * }
+ *//*
 	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
 	 * menu items for use in the action bar MenuInflater inflater =
 	 * getMenuInflater(); inflater.inflate(R.menu.main_activity_actions, menu);
 	 * // Get the SearchView and set the searchable configuration MenuItem
-	 * searchItem=menu.findItem(R.id.action_search); SearchView
-	 * searchView=(SearchView)MenuItemCompat.getActionView(searchItem);
-	 * SearchManager searchManager = (SearchManager)
-	 * getSystemService(Context.SEARCH_SERVICE); //SearchView searchView =
-	 * (SearchView) menu.findItem(R.id.action_search) // .getActionView(); //
-	 * Assumes current activity is the searchable activity
+	 * searchItem = menu.findItem(R.id.action_search); SearchView searchView =
+	 * (SearchView) MenuItemCompat .getActionView(searchItem); SearchManager
+	 * searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+	 * // SearchView searchView = (SearchView) //
+	 * menu.findItem(R.id.action_search) // .getActionView(); // Assumes current
+	 * activity is the searchable activity
 	 * searchView.setSearchableInfo(searchManager
 	 * .getSearchableInfo(getComponentName()));
-	 * searchView.setIconifiedByDefault(false); // Do not iconify the widget; //
-	 * expand it by default // searchView.setSubmitButtonEnabled(true); return
-	 * true; }
-	 *//*
-		 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate
-		 * the menu items for use in the action bar MenuInflater inflater =
-		 * getMenuInflater(); inflater.inflate(R.menu.main_activity_actions,
-		 * menu); // Get the SearchView and set the searchable configuration
-		 * MenuItem searchItem = menu.findItem(R.id.action_search); SearchView
-		 * searchView = (SearchView) MenuItemCompat .getActionView(searchItem);
-		 * SearchManager searchManager = (SearchManager)
-		 * getSystemService(Context.SEARCH_SERVICE); // SearchView searchView =
-		 * (SearchView) // menu.findItem(R.id.action_search) //
-		 * .getActionView(); // Assumes current activity is the searchable
-		 * activity searchView.setSearchableInfo(searchManager
-		 * .getSearchableInfo(getComponentName()));
-		 * searchView.setIconifiedByDefault(false); // Do not iconify the //
-		 * widget; // expand it by default //
-		 * searchView.setSubmitButtonEnabled(true);
-		 * 
-		 * return true; }
-		 */
+	 * searchView.setIconifiedByDefault(false); // Do not iconify the // widget;
+	 * // expand it by default // searchView.setSubmitButtonEnabled(true);
+	 * 
+	 * return true; }
+	 */
--- src/com/technionrankerv1/SearchableAdapter.java
+++ src/com/technionrankerv1/SearchableAdapter.java
@@ -15,49 +15,44 @@ import android.widget.TextView;
 class SearchableAdapter extends BaseAdapter {
 	private static LayoutInflater inflater = null;
 	private int count;
-	
+
 	private ArrayList<String> filteredData = null;
 	// private ItemFilter mFilter = new ItemFilter();
 	String contstaint;
 	ArrayList<String> values;
-	String[] list=null;
+	String[] list = null;
 
 	// public ImageLoader imageLoader;
 
-	public SearchableAdapter(String constraint, Activity activity, String[] professorsAndCourses) {
-		
+	public SearchableAdapter(String constraint, Activity activity,
+			String[] professorsAndCourses) {
+
 		// values = new ArrayList<String>();
 		// values.add("found1");
 		// values.add("found2");
 		list = professorsAndCourses;
 		/*
-		AutoCorrect ac = new AutoCorrect();
-		String[] corArray = null;
-		String[] proArray = null;
-		
-		try {
-			Log.d("MyApp", "Trying to parse.");
-
-			corArray = ac.parseCourses();
-			Log.d("MyApp", "Did i parse anything?");
-
-			proArray = ac.parseProfessors();
-			Log.d("MyApp", "No way I parsed everything.");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		this.list = ac.concat(corArray, proArray);
-		*/
+		 * AutoCorrect ac = new AutoCorrect(); String[] corArray = null;
+		 * String[] proArray = null;
+		 * 
+		 * try { Log.d("MyApp", "Trying to parse.");
+		 * 
+		 * corArray = ac.parseCourses(); Log.d("MyApp",
+		 * "Did i parse anything?");
+		 * 
+		 * proArray = ac.parseProfessors(); Log.d("MyApp",
+		 * "No way I parsed everything.");
+		 * 
+		 * } catch (Exception e) { e.printStackTrace(); }
+		 * 
+		 * this.list = ac.concat(corArray, proArray);
+		 */
 		ItemFilter fil = new ItemFilter(list);
-		Log.d("MyApp", "Back from filter1.");
 
 		values = fil.mich(constraint);
-		Log.d("MyApp", "Back from filter2.");
+		
 
 		count = values.size();
-
 		this.contstaint = constraint;
 		this.inflater = (LayoutInflater) activity
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -80,50 +75,51 @@ class SearchableAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		View vi = convertView;
-		if (convertView == null)
-			vi = inflater.inflate(R.layout.list_item, null);
 
-		TextView name = (TextView) vi.findViewById(R.id.lblListItem);
-		name.setText(values.get(position));
-		// imageLoader.DisplayImage(song.get(CustomizedListView.KEY_THUMB_URL),
-		// thumb_image);
-		return vi;
+		if (getCount() != 0) {
+			View vi = convertView;
+			if (convertView == null)
+				vi = inflater.inflate(R.layout.list_item, null);
+
+			TextView name = (TextView) vi.findViewById(R.id.lblListItem);
+			name.setText(values.get(position));
+			// imageLoader.DisplayImage(song.get(CustomizedListView.KEY_THUMB_URL),
+			// thumb_image);
+
+			return vi;
+
+		} else {
+		}
+
+		return convertView;
 	}
 
 	private class ItemFilter extends Filter {
-		String [] list1=null;
+		String[] list1 = null;
+
 		public ItemFilter(String[] list) {
-			list1=list;
+			list1 = list;
 		}
 
 		public FilterResults performFiltering(CharSequence constraint) {
-			Log.d("MyApp", "Im fucking Filtering");
 
 			String filterString = constraint.toString().toLowerCase();
-			Log.d("MyApp", "Im fucking Filtering1");
 
 			FilterResults results = new FilterResults();
-			Log.d("MyApp", "Im fucking Filtering2");
 
-			
 			int count = list1.length;
-			Log.d("MyApp", "count is:"+ count);
 
 			final ArrayList<String> nlist = new ArrayList<String>(count);
-			Log.d("MyApp", "Im fucking Filtering3");
+			
 
 			for (int i = 0; i < list1.length; i++) {
-				if (list1[i].toLowerCase().contains(constraint)) {
+				if (list1[i].toLowerCase().contains(filterString)) {
 					nlist.add(list1[i]);
 				}
 			}
-			Log.d("MyApp", "Im fucking Filtering4");
 
 			results.values = nlist;
 			results.count = nlist.size();
-			Log.d("MyApp", "Im done Filtering");
-
 			return results;
 		}
 
